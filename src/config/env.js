01ee1bf8fa3b0af25c338c1f79f8488aad979077
Umang Chopra <60170659+SoftwareSolutions-arch@users.noreply.github.com>
2025-03-07 15:36:24 +0530

    code

--- src/config/env.js
+++ src/config/env.js
@@ -5,11 +5,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
   development: {
     port: process.env.PORT || 3000,
-    db: process.env.DB_CONNECTION_STRING,
+    db: 'mongodb+srv://user123:1GpJ1IR1d9ZO9IZe@cluster0.whnf4.mongodb.net/',
   },
   production: {
-    port: process.env.PORT,
-    db: process.env.DB_CONNECTION_STRING,
+    port: process.env.PORT || 3000,
+    db: 'mongodb+srv://user123:1GpJ1IR1d9ZO9IZe@cluster0.whnf4.mongodb.net/',
   }
 };
 
