89041026f27c23a0a82bc8e59f749c213e4a8971
Umang Chopra <60170659+SoftwareSolutions-arch@users.noreply.github.com>
2025-03-07 18:14:15 +0530

    nodemon

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ connectDB();
 
 // CORS Middleware Configuration
 app.use(cors({
-  origin: 'https://ez-onsite-frontend.vercel.app', // Allow requests from Angular frontend
+  origin: ["http://localhost:4200", "https://ez-onsite-frontend.vercel.app"], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
   credentials: true // If using cookies or authentication
 }));
-
+  
 // Middleware
 app.use(express.json());
 
